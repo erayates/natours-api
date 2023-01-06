1139cdd917ecd7bf8c2166bfc14cb797ca86b104
Eray Ateş <bligzer@outlook.com>
2023-01-07 02:19:56 +0300

    post and respond to url parameters done.

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const fs = require('fs');
 
 const port = 3000;
 
+
+app.use(express.json())
+
+
+
 // GET Request
 // app.get('/', (req,res) => {
 //     res.status(200).json(
@@ -29,6 +34,42 @@ app.get('/api/v1/tours', (req,res) => {
     })
 })
 
+app.get('/api/v1/tours/:id', (req,res) => {
+    console.log(req.params)
+ 
+    const tour = tours.find(el => el.id === parseInt(req.params.id))
+    if(!tour) return res.status(404).json({
+        status: 'fail',
+        message: 'There is no tour with that ID.'
+    })
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+})
+
+
+
+
+
+app.post('/api/v1/tours', (req,res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+
+    // Always send a response
+})
 
 
 app.listen(port,() => {
