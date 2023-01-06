a867cedeb21329f4f59ee797f42a6853635cde08
Eray Ateş <bligzer@outlook.com>
2023-01-06 10:35:15 +0300

    Tours API endpoint created.

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+const port = 3000;
+
+// GET Request
+// app.get('/', (req,res) => {
+//     res.status(200).json(
+//         {message: 'Hello from the server side!', app: 'Natours'}
+//     );
+// })
+
+// POST
+// app.post('/', (req,res) => {
+//     res.send('You can post to this endpoint...')
+// })
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+app.get('/api/v1/tours', (req,res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours
+        }
+    })
+})
+
+
+
+app.listen(port,() => {
+    console.log(`App running on port ${port}...`)
+})
