122b98d8de4cc8d37a8add14228a71021d8b63a3
Eray Ateş <bligzer@outlook.com>
2023-02-04 01:41:27 +0300

    chaining middleware function added. (checkBody))

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,6 +21,15 @@ exports.checkID = (req, res, next, val)=> {
     next();
 }
 
+exports.checkBody = (req,res,next) => {
+    if(!req.body.name || !req.body.price) return res.status(400).json({
+        status: 'fail',
+        message: 'Missing name or price'
+    });
+    next();
+}
+
+
 // GET Request with ID
 
 exports.getTour = (req,res) => {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,22 @@ const router = express.Router();
 
 router.param('id', tourController.checkID)
 
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
+
 // Routes
+
+
 
 router
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour)
+    .post(tourController.checkBody,tourController.createTour) // This is a middleware stack (chaining multiple middleware functions)
+    // First tourController.checkBody is executed, then tourController.createTour
+    // If tourController.checkBody returns next(), then tourController.createTour is executed
     
 
 router
