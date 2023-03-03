888c55dd28d2c964be403a440d610b69c74f7a01
Eray Ateş <bligzer@outlook.com>
2023-03-03 16:20:17 +0300

    advanced filtering codes added.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,15 +5,26 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req,res) => {
     try{
+        // BUILD QUERY
+        // 1) Filtering
         const queryObj = {...req.query};
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach(el => delete queryObj[el]);
-        const tours = await Tour.find(queryObj);
+        // const tours = await Tour.find(queryObj);
         /* OR
         const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
         */
 
-     
+        // 2) Advanced Filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+        const query = Tour.find(JSON.parse(queryStr))
+        
+
+        // EXECUTE QUERY
+        const tours = await query;
+
+
         res.status(200).json({
             status: 'success',
             results: tours.length,
