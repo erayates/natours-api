76062e4f0f197ba8829e9a28e4905bf909456cc5
Eray Ateş <bligzer@outlook.com>
2023-03-09 01:44:23 +0300

    api controller refactoring process done.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,6 @@
-const Tour = require('./../models/tourModel');
+const Tour = require('../models/tourModel');
 
+const APIFeatures = require('../utilities/apiFeatures');
 
 // const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
@@ -11,66 +12,17 @@ exports.aliasTopTours = async(req,res,next) => {
 }
 
 
+
+
 exports.getAllTours = async (req,res) => {
     try{
-        // BUILD QUERY
-
-        // 1) Filtering
-        const queryObj = {...req.query};
-        const excludedFields = ['page', 'sort', 'limit', 'fields'];
-        excludedFields.forEach(el => delete queryObj[el]);
-        // const tours = await Tour.find(queryObj);
-        /* OR
-        const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
-        */
-
-        // 2) Advanced Filtering
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-
-        // 3) Sorting
-      
-        let query = Tour.find(JSON.parse(queryStr));
-        if(req.query.sort){
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-
-        }else{
-            query.sort('-createdAt');
-        }
-
-        // 4) Field Limiting
-        if(req.query.fields){
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        }else{
-            query = query.select('-__v');
-        }
-
-        // 5) Pagination
-        const page = req.query.page * 1 || 1;
-        const limit = req.query.limit * 1 || 100;
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if(req.query.page){
-            const numTours = await Tour.countDocuments(); // Tour.countDocuments() ile toplam tour sayısı bulunur. Bir promise dönderir, o yüzden await kullanılır.
-            if(skip >= numTours) throw new Error('This page does not exists.'); // Direkt olarak res.status.404 yani catch bloğuna gider.
-        }
-
-        // 6) Aliasing
-
-
-
-
-        // the text has special characters think how you can just get only the words
-        // ["I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"]
-        // const query = Tour.find(JSON.parse(queryStr))
-        
-
         // EXECUTE QUERY
-        const tours = await query;
+        const features = new APIFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .pagination();
+        const tours = await features.query;
 
 
         res.status(200).json({
@@ -193,7 +145,10 @@ exports.updateTour = (req,res) => {
             }
         })
     }catch(err){
-
+        res.status(404).json({
+            status: 'fail',
+            message: 'There is no tour with that ID.'
+        })
     }
     res.status(200).json({
         status: 'success',
